f403025f85192e3d0578be8c14b9f24ddf494a68
pawansarap <pawansarap519@gmail.com>
2024-05-11 23:19:51 +0530

    added more conditonal rendering for clear button and sort fillter

--- src/components/PackingList.js
+++ src/components/PackingList.js
@@ -36,14 +36,14 @@ export default function PackingList({
         ))}
       </ul>
 
-      <div className="actions">
+     { sortedItems.length !== 0 && <div className="actions">
         <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
           <option value="input">Sort by input order</option>
           <option value="description">Sort by description</option>
           <option value="packed">Sort by packed status</option>
         </select>
         <button onClick={onClearList}>Clear list</button>
-      </div>
+      </div>}
     </div>
   );
 }
